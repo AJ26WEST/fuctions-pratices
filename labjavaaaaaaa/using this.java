a9cd6bf92942e6b4a999b9a7b2b7d91ddfb1c63e
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-10-12 22:06:32 +0530

    Update using this.java

--- labjavaaaaaaa/using this.java
+++ labjavaaaaaaa/using this.java
@@ -1,1 +1,30 @@
+class Main {
+
+    public static void main(String[] args) {
+        Student abhi = new Student();
+        var v = .2f;
+        abhi.setDetails(21, "abhishek",67.9f);
+        abhi.displayDetails();
+    }
+}
+
+class Student {
+    int rnoll;
+    String name;
+    Float marks ;
+
+    // Method to set details using 'this' keyword
+    void setDetails(int rnoll, String name,Float marks) {
+        this.rnoll = rnoll; // 'this' refers to the current object instance
+        this.name = name;
+        this.marks = marks ;
+    }
+
+    // Method to display details
+    void displayDetails() {
+        System.out.println(this.rnoll); // Optional use of 'this'
+        System.out.println(this.name);
+        System.out.println(this.marks);// Optional use of 'this'
+    }
+}
 
