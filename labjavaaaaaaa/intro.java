cf0c84d5f19022b7a42f7d2698f8b511cdb57be9
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-10-12 15:00:17 +0530

    Create intro.java

---
+++ labjavaaaaaaa/intro.java
@@ -0,0 +1,14 @@
+class main {
+
+    public static void main(String[] args) {
+        Student abhi = new Student();
+        abhi.rnoll = 21;
+        abhi.name = "abhishek" ;
+        System.out.println(abhi.rnoll);
+        System.out.println(abhi.name);
+    }
+}
+ class Student{
+    int rnoll;
+    String name;
+ }
